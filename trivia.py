7bfdd284e20fab34b2fcf3dbc2f1145b481e8188
Pqhantom <41537848+Pqhantom@users.noreply.github.com>
2022-04-30 06:50:50 +0000

    rng stuff

--- trivia.py
+++ trivia.py
@@ -1,3 +1,5 @@
+import random
+
 qadb = []
 qadb.append({
   "Question": "What animal goes Meow?",
@@ -7,12 +9,25 @@ qadb.append({
   "Question": "What animal goes Moo?",
   "Answer": "cow"
 })
+qadb.append({
+  "Question": "What animal goes woof?",
+  "Answer": "dog"
+})
+# ~~~~~  This is a tester to print the Answer Key in the terminal ~~~~~
 
-def printing(n): # this printes the code with the format given 
-  print(qadb[n]["Question"])
-  print(qadb[n]["Answer"])
+# def printing(n): # this printes the code with the format given 
+#   print(qadb[n]["Question"])
+#   print(qadb[n]["Answer"])
               
-def aba(): #simple function that prints out all of the Q&A entries 
-  for n in range(len(qadb)):
-    printing(n)
+# def aba(): #simple function that prints out all of the Q&A entries 
+#   for n in range(len(qadb)):
+#     printing(n)
 
+def brain():
+  n = random.randint(0, 2)
+  question = qadb[n]["Question"]
+  print(n)
+  print(question)
+ 
+
+brain()
